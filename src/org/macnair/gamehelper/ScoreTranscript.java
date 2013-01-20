a392310791f8b95341b195d466a0f8a1c174a549
Michael Macnair <michael@macnair.org>
2013-01-20 19:55:12 +0000

    stash animation progress prior to removal

---
+++ src/org/macnair/gamehelper/ScoreTranscript.java
@@ -0,0 +1,92 @@
+package org.macnair.gamehelper;
+
+import java.util.ArrayList;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * A fragment representing a single Helper detail screen. This fragment is
+ * contained in a {@link HelperListActivity}
+ */
+public class ScoreTranscript extends Fragment implements OnClickListener, OnKeyListener {
+	static final String TAG = "ScoreTranscript";
+	private EditText mAddScoreText;
+	private ArrayAdapter<Integer> mAdapter;
+	private ArrayList<Integer> mScores = new ArrayList<Integer>();
+	private ListView mTranscript;
+	
+	/**
+	 * Mandatory empty constructor for the fragment manager to instantiate the
+	 * fragment (e.g. upon screen orientation changes).
+	 */
+	public ScoreTranscript() {
+	}
+	
+	/** Called when the user touches the sub-total button */
+	public void transcriptSubtotal(View view) {
+		Log.d(TAG,String.format("Subtotal: %d", 1));
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		View rootView = inflater.inflate(R.layout.score_transcript, container, false);
+		
+        mAdapter = new ArrayAdapter<Integer>(getActivity(), android.R.layout.simple_list_item_1, mScores);
+        mTranscript = (ListView) rootView.findViewById(R.id.list_transcript);
+        mTranscript.setAdapter(mAdapter);
+        
+        mAddScoreText = (EditText) rootView.findViewById(R.id.text_transcript_input);
+        mAddScoreText.setOnClickListener(this);
+        mAddScoreText.setOnKeyListener(this);
+		
+		ListView transcript = (ListView) rootView.findViewById(R.id.list_transcript);
+		
+		transcript.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = ((TextView)view).getText().toString();
+                Log.d(TAG,String.format("Item clicked: %s", item));
+			}
+		});
+		
+		return rootView;
+	}
+	
+	private void addScore() {
+	    String text = mAddScoreText.getText().toString();
+	    mAdapter.add(Integer.parseInt(text));
+	    mAddScoreText.setText(null);
+	}
+	 
+	public void onClick(View v) {
+		addScore();
+    }
+	
+	public boolean onKey(View v, int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_DPAD_CENTER:
+                case KeyEvent.KEYCODE_ENTER:
+                	addScore();
+                    return true;
+            }
+        }
+        return false;
+    }
+}
